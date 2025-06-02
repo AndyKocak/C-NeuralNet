8a1259d3208d196511831dc7e70a22c6978a8409
AndyKocak <andykocak0@gmail.com>
2025-06-01 21:06:23 -0400

    Added raw_conversion

--- data/emotion/convert.py
+++ data/emotion/convert.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import os
 
+import numpy as np
+
 #small script to convert the png fer data into .raw for easier reading in c file
 def convert_to_raw(input_dir, output_dir, size=(48, 48)):
     if not os.path.exists(output_dir):
@@ -11,5 +13,22 @@ def convert_to_raw(input_dir, output_dir, size=(48, 48)):
             img_path = os.path.join(input_dir, img_file)
             img = Image.open(img_path).convert('L').resize(size)  # Convert to grayscale and resize
             raw_path = os.path.join(output_dir, os.path.splitext(img_file)[0] + '.raw')
-            img.save(raw_path, format='RAW')
+            img_arr = np.array(img)
+            raw_data = img_arr.flatten()
+            raw_bytes = raw_data.tobytes()
+            with open(raw_path, 'wb') as f:
+                f.write(raw_bytes)
 
+#convert_to_raw("test/angry", "test/angry_raw")
+#convert_to_raw("test/disgusted", "test/disgusted_raw")
+#convert_to_raw("test/fearful", "test/fearful_raw")
+#convert_to_raw("test/happy", "test/happy_raw")
+#convert_to_raw("test/neutral", "test/neutral_raw")
+#convert_to_raw("test/sad", "test/sad_raw")
+#convert_to_raw("test/surprised", "test/surprised_raw")
+
+# Example loading from a file
+with open("test/sad_raw/im0.raw", "rb") as f:
+   raw_data = f.read()
+   img = Image.frombytes('L', (48, 48), raw_data)
+   img.show()
